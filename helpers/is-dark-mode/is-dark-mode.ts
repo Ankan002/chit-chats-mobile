ecb00893e07a26386698afd68177709bda2904f8
Ankan Bhattacharya <ankanbhattacharya@Ankans-MacBook-Pro.local>
2022-04-19 10:32:28 +0530

    chnage default theme

--- helpers/is-dark-mode/is-dark-mode.ts
+++ helpers/is-dark-mode/is-dark-mode.ts
@@ -4,8 +4,8 @@ export const getLastTheme = async (): Promise<boolean> => {
     const darkMode = await AsyncStorage.getItem("dark-mode");
 
     if(!darkMode) {
-        AsyncStorage.setItem("dark-mode", JSON.stringify(false));
-        return false;
+        AsyncStorage.setItem("dark-mode", JSON.stringify(true));
+        return true;
     }
 
     return JSON.parse(darkMode);
